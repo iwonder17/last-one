5403020569365796a353894a34cd92331d6f050e
iwonder17 <136564837+iwonder17@users.noreply.github.com>
2023-06-22 17:19:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var button = document.getElementById('bar');
+
+// Add a click event listener to the button
+button.addEventListener('click', function () {
+  'use strict';
+  // Display the letter 'A' on the console
+  console.log('A');
+});

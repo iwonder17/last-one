3caed0b78aac4520c525ff397ca7764dafd13196
iwonder17 <136564837+iwonder17@users.noreply.github.com>
2023-06-22 18:54:26 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var button = document.getElementById('bar');
-
-// Add a click event listener to the button
-button.addEventListener('click', function () {
-  'use strict';
-  // Display the letter 'A' on the console
-  console.log('A');
-});

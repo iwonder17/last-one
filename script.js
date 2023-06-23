c7ca52b9a3120104826a9f4562915aed4cdb718a
iwonder17 <136564837+iwonder17@users.noreply.github.com>
2023-06-23 21:48:17 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function closeallshit() {
     
-  document.getElementById('about').style.display= 'none'; document.getElementById('contact').style.display= 'none'; document.getElementById('services').style.display= 'none';   
+  document.getElementById('about').style.display= 'none'; 
     
-    document.getElementById('motherlist').style.display= 'none';   
+ document.getElementById('motherlist').style.display= 'none';   
 }
 
 
 function hidemotherlist() {
   
-    closeallshit();document.getElementById('motherlist').style.display= 'block';    
+    closeallshit();
+    //document.getElementById('motherlist').style.height= '0px';  
+    document.getElementById('motherlist').style.display= 'block';   
+     //document.getElementById('motherlist').style.height= '85px';  
 }// button
 
 
@@ -16,14 +19,190 @@ function hidemotherlist() {
 function aboutcontent() {
 closeallshit(); 
 document.getElementById('about').style.display= 'block'; 
-}
+    hi('i am loading at 1');
+    hi2('i am loading at 1');
+    hi3('i am loading at 1');
+    hi4('i am loading at 1');
+    hi5('i am loading at 1');
+    hi6('i am loading at 1');
+}//about
+
 function contactcontent() {
 closeallshit(); 
-document.getElementById('contact').style.display= 'block'; 
-}
+document.getElementById('about').style.display= 'block'; 
+    hi('i am loading at 2');
+    hi2('i am loading at 2');
+    hi3('i am loading at 2');
+    hi4('i am loading at 2');
+    hi5('i am loading at 2');
+    hi6('i am loading at 2');
+    
+}//contact
 function servicescontent() {
 closeallshit(); 
-document.getElementById('services').style.display= 'block'; 
-}
+document.getElementById('about').style.display= 'block'; 
+    hi('i am loading at 3i am loading at 3i am loading at 3i am loading at 3i am loading at 3i am loading at 3i am loading at 3i am loading at 3i am loading at 3i am loading at 3i am loading at 3i am loading at 3i am loading at 3i am loading at 3i am loading at 3i am loading at 3i am loading at 3i am loading at 3i am loading at 3');
+    hi2('i am loading at 3');
+    hi3('i am loading at 3');
+    hi4('i am loading at 3');
+    hi5('i am loading at 3');
+    hi6('i am loading at 3');
+}//services
+function homecontent() {
+closeallshit(); 
+document.getElementById('about').style.display= 'block'; 
+    hi('i am loading at 2');
+    hi2('i am loading at 2');
+    hi3('i am loading at 2');
+    hi4('i am loading at 2');
+    hi5('i am loading at 2');
+    hi6('i am loading at 2');
+    
+}//contact
+ 
+
+
+// không tìm thấy id 
+
+{
+function hi(sentences) {
+  fff1.innerHTML = "";
+  for (var i = 0; i < sentences.length; i++) {
+    var words = sentences[i].split("");
+    for (var j = 0; j < words.length; j++) {
+      setTimeout(function(word) {
+        fff1.innerHTML += word;
+      }, (1 * j+(30 * i)) , words[j]);
+    }
+  }
+};
+////////////////////////////////////////////
+function hi2(sentences) {
+  fff2.innerHTML = "";
+  for (var i = 0; i < sentences.length; i++) {
+    var words = sentences[i].split("");
+    for (var j = 0; j < words.length; j++) {
+      setTimeout(function(word) {
+        fff2.innerHTML += word;
+      }, (1 * j+(30 * i)) , words[j]);
+    }
+  }
+};
+////////////////////////////////////////////
+function hi3(sentences) {
+  fff3.innerHTML = "";
+  for (var i = 0; i < sentences.length; i++) {
+    var words = sentences[i].split("");
+    for (var j = 0; j < words.length; j++) {
+      setTimeout(function(word) {
+        fff3.innerHTML += word;
+      }, (1 * j+(30 * i)) , words[j]);
+    }
+  }
+};
+////////////////////////////////////////////
+function hi4(sentences) {
+  fff4.innerHTML = "";
+  for (var i = 0; i < sentences.length; i++) {
+    var words = sentences[i].split("");
+    for (var j = 0; j < words.length; j++) {
+      setTimeout(function(word) {
+        fff4.innerHTML += word;
+      }, (1 * j+(30 * i)) , words[j]);
+    }
+  }
+};
+////////////////////////////////////////////
+function hi5(sentences) {
+  fff5.innerHTML = "";
+  for (var i = 0; i < sentences.length; i++) {
+    var words = sentences[i].split("");
+    for (var j = 0; j < words.length; j++) {
+      setTimeout(function(word) {
+        fff5.innerHTML += word;
+      }, (1 * j+(30 * i)) , words[j]);
+    }
+  }
+};
+////////////////////////////////////////////
+function hi6(sentences) {
+  fff6.innerHTML = "";
+  for (var i = 0; i < sentences.length; i++) {
+    var words = sentences[i].split("");
+    for (var j = 0; j < words.length; j++) {
+      setTimeout(function(word) {
+        fff6.innerHTML += word;
+      }, (1 * j+(30 * i)) , words[j]);
+    }
+  }
+};
+////////////////////////////////////////////
+function hi7(sentences) {
+  fff7.innerHTML = "";
+  for (var i = 0; i < sentences.length; i++) {
+    var words = sentences[i].split("");
+    for (var j = 0; j < words.length; j++) {
+      setTimeout(function(word) {
+        fff7.innerHTML += word;
+      }, (1 * j+(30 * i)) , words[j]);
+    }
+  }
+};
+////////////////////////////////////////////
+function hi8(sentences) {
+  fff8.innerHTML = "";
+  for (var i = 0; i < sentences.length; i++) {
+    var words = sentences[i].split("");
+    for (var j = 0; j < words.length; j++) {
+      setTimeout(function(word) {
+        fff8.innerHTML += word;
+      }, (1 * j+(30 * i)) , words[j]);
+    }
+  }
+};
+////////////////////////////////////////////
+function hi9(sentences) {
+  fff9.innerHTML = "";
+  for (var i = 0; i < sentences.length; i++) {
+    var words = sentences[i].split("");
+    for (var j = 0; j < words.length; j++) {
+      setTimeout(function(word) {
+        fff9.innerHTML += word;
+      }, (1 * j+(30 * i)) , words[j]);
+    }
+  }
+};
+////////////////////////////////////////////
+function hi10(sentences) {
+  fff10.innerHTML = "";
+  for (var i = 0; i < sentences.length; i++) {
+    var words = sentences[i].split("");
+    for (var j = 0; j < words.length; j++) {
+      setTimeout(function(word) {
+        fff10.innerHTML += word;
+      }, (1 * j+(30 * i)) , words[j]);
+    }
+  }
+};
+////////////////////////////////////////////
+function hi11(sentences) {
+  fff11.innerHTML = "";
+  for (var i = 0; i < sentences.length; i++) {
+    var words = sentences[i].split("");
+    for (var j = 0; j < words.length; j++) {
+      setTimeout(function(word) {
+        fff11.innerHTML += word;
+      }, (1 * j+(30 * i)) , words[j]);
+    }
+  }
+};
+////////////////////////////////////////////
+
+
+}// buch of shit for loading text
+
+
+
+
 
 

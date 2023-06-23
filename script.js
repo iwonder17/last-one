4406b5428a17405392e2f38677df10ab169bfbb6
iwonder17 <136564837+iwonder17@users.noreply.github.com>
2023-06-23 16:45:13 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-  console.log("Hi");
-document.getElementById('motherlist').style.display= 'none';
+function closeallshit() {
+    
+  document.getElementById('about').style.display= 'none'; document.getElementById('contact').style.display= 'none'; document.getElementById('services').style.display= 'none';   
+    
+    document.getElementById('motherlist').style.display= 'none';   
+}
+
+
+function hidemotherlist() {
+  
+    closeallshit();document.getElementById('motherlist').style.display= 'block';    
+}// button
+
+
+
+function aboutcontent() {
+closeallshit(); 
+document.getElementById('about').style.display= 'block'; 
+}
+function contactcontent() {
+closeallshit(); 
+document.getElementById('contact').style.display= 'block'; 
+}
+function servicescontent() {
+closeallshit(); 
+document.getElementById('services').style.display= 'block'; 
+}
+
+
